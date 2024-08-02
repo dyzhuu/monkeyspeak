ad2a9a1d399dd713670ac52f48ee04f993a9da92
Yoyo <yoyoc.pata@gmail.com>
2024-08-03 01:35:34 +1200

    Updated 'speaking page'
    Set up the layout and style for the basic functions the page requires (responsive I think).
    Need to do:
    - Change the font to 'Kumbh Sans'
    - I had set the text overflow to be hidden, need to find out how 'scrolling' works when text gets to needing to scroll.
    - THIS IS STATIC
    The start button is suppose to be pressed, countdown/timer replaces the spot of the button.

--- app/components/textbox.tsx
+++ app/components/textbox.tsx
@@ -1,7 +1,7 @@
 export default function TextBox() {
     return (
-        <div className='w-full'>
-            <textarea className='w-full min-h-[25vh]' placeholder='Monkey go brr...'/>
+        <div className='w-full pb-8'>
+            <p className='text-justify max-w-[75rem] w-full h-60 text-[2rem] text-[#394760] overflow-y-hidden'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import TextBox from './components/textbox'
 
 export default function Home() {
     return (
-        <div className='min-h-screen flex flex-col items-center bg-zinc-600 p-10'>
-            <h1 className='m-5 font-bold text-5xl text-gray-300'>Monkey Speak 🐒</h1>
-            
-            <div className='w-3/4 flex flex-col justify-center items-center rounded-lg bg-gray-200 m-5 p-5'>
+        <div className='min-h-screen flex flex-col bg-[#0B0E13] p-10'>
+
+            <div className='flex flex-col self-center justify-center m-5 bg-opacity-0'>
+            <h1 className='font-bold text-5xl text-[#9FADC6] pb-[2rem]'>monkeyspeak</h1>
                 <TextBox />
                 
-                <div className='w-full flex mt-5 bg-gray-200'>
-                    <button className='rounded-md font-bold text-white bg-gray-400 p-2'>Start</button>
+                <div className='w-full justify-center flex mt-5'>
+                    <button className='rounded-[5rem] w-64 h-24 font-bold text-[#394760] text-[2rem] bg-[#9FADC6] item-center'>Start</button>
                 </div>
             </div>
         </div>
