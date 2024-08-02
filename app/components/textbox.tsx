b0193b66b2d5780e7669ddd3f95e8f78c0e789f4
f13322 <119908908+f13322@users.noreply.github.com>
2024-08-03 04:30:50 +1200

    Made text box text dynamic
    Made the texts in the text box able to be coloured dynamically accordingly.

--- app/components/textbox.tsx
+++ app/components/textbox.tsx
@@ -1,7 +1,9 @@
+import { done, not_done } from './test'
+
 export default function TextBox() {
     return (
         <div className='w-full pb-8'>
-            <p className='text-justify max-w-[75rem] w-full h-60 text-[2rem] text-[#394760] overflow-y-hidden'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className='text-justify max-w-[75rem] w-full h-60 text-[2rem] text-[#394760] overflow-y-hidden'><em className='text-justify max-w-[75rem] w-full h-60 text-[2rem] text-[#F94760] overflow-y-hidden'>{done}</em>{not_done}</p>
         </div>
     )
 }
