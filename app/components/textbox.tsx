7ee904092d96e585795c6bfe0fc6439c70fe2f58
Ethan Quach <kqua446@aucklanduni.ac.nz>
2024-08-03 15:01:32 +1200

    - check function - ran prettier

--- app/components/textbox.tsx
+++ app/components/textbox.tsx
@@ -1,9 +1,14 @@
-import { done, not_done } from './test'
+import { done, not_done } from './test';
 
 export default function TextBox() {
-    return (
-        <div className='w-full pb-8'>
-            <p className='text-justify max-w-[75rem] w-full h-60 text-[2rem] text-[#394760] overflow-y-hidden'><em className='text-justify max-w-[75rem] w-full h-60 text-[2rem] text-[#F94760] overflow-y-hidden'>{done}</em>{not_done}</p>
-        </div>
-    )
+  return (
+    <div className="w-full pb-8">
+      <p className="text-justify max-w-[75rem] w-full h-60 text-[2rem] text-[#394760] overflow-y-hidden">
+        <em className="text-justify max-w-[75rem] w-full h-60 text-[2rem] text-[#F94760] overflow-y-hidden">
+          {done}
+        </em>
+        {not_done}
+      </p>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
-import TextBox from './components/textbox'
+import TextBox from './components/textbox';
 
 export default function Home() {
-    return (
-        <div className='min-h-screen flex flex-col bg-[#0B0E13] p-10'>
+  return (
+    <div className="min-h-screen flex flex-col bg-[#0B0E13] p-10">
+      <div className="flex flex-col self-center justify-center m-5 bg-opacity-0">
+        <h1 className="font-bold text-5xl text-[#9FADC6] pb-[2rem]">
+          monkeyspeak
+        </h1>
+        <TextBox />
 
-            <div className='flex flex-col self-center justify-center m-5 bg-opacity-0'>
-            <h1 className='font-bold text-5xl text-[#9FADC6] pb-[2rem]'>monkeyspeak</h1>
-                <TextBox />
-                
-                <div className='w-full justify-center flex mt-5'>
-                    <button className='rounded-[5rem] w-64 h-24 font-bold text-[#394760] text-[2rem] bg-[#9FADC6] item-center'>Start</button>
-                </div>
-            </div>
+        <div className="w-full justify-center flex mt-5">
+          <button className="rounded-[5rem] w-64 h-24 font-bold text-[#394760] text-[2rem] bg-[#9FADC6] item-center">
+            Start
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
