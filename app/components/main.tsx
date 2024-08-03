a5b49a71571b5f52fb81af742b428eae4a4a80d8
CMCheckmate <mingraygoy@gmail.com>
2024-08-03 13:40:18 +1200

    Adding fade

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,19 +1,28 @@
 'use client'
 
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import TextBox from './textbox';
 import Results from './results';
 
 export default function Main() {
+    const textBox = useRef<HTMLDivElement>(null);
     const [gameOver, setGameOver] = useState(false);
+
+    function toggleAnimation() {
+        const textDiv = textBox.current as HTMLDivElement;
+
+        textDiv.classList.remove('animate-[fade_0.5s]');
+        textDiv.offsetHeight;
+        textDiv.classList.add('animate-[fade_0.5s]');
+    }
 
     return (
         <div className='w-3/4 min-w-max m-16'>
             <h1 className='font-bold text-5xl text-[#9FADC6] m-2'>monkeyspeak</h1>
             
             <div className='w-full flex flex-col justify-center items-center gap-5'>
-                <div className='w-full min-h-[25vh] grid'>
+                <div ref={textBox} onAnimationEnd={() => { setGameOver(!gameOver); } } className='w-full min-h-[25vh] grid'>
                     {
                         gameOver ?
                             <Results />
@@ -23,7 +32,7 @@ export default function Main() {
                 </div>
 
                 <div className='w-full flex justify-center items-center '>
-                    <button onClick={() => { setGameOver(!gameOver); }} className='rounded-[5rem] font-bold text-2xl text-[#394760] bg-[#9FADC6] p-2 px-5'>{gameOver ? 'Restart' : 'Start'}</button>
+                    <button onClick={toggleAnimation} className='rounded-[5rem] font-bold text-2xl text-[#394760] bg-[#9FADC6] p-2 px-5'>{gameOver ? 'Restart' : 'Start'}</button>
                 </div>
             </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,11 +66,15 @@ export default {
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' }
+        },
+        'fade': {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0.5' }
         }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
+        'accordion-up': 'accordion-up 0.2s ease-out',
       }
     }
   },
