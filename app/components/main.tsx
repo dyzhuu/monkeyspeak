daed345953ac1e16b5e2cce22221b43d8ef34d7f
f13322 <119908908+f13322@users.noreply.github.com>
2024-08-04 02:09:58 +1200

    different view depending on if multiplayer

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -8,6 +8,8 @@ import MultiplayerBars from './multiplayerBars';
 import Loading from './loading';
 
 import { generateRandomParagraph } from '../hooks/word_generator';
+import progressBar from './progressBar';
+import { User } from 'lucide-react';
 
 export default function Main() {
     const textBox = useRef<HTMLDivElement>(null);
@@ -16,6 +18,7 @@ export default function Main() {
     const [gameRunning, setGameRunning] = useState(false);
     const [timer, setTimer] = useState(0);
     const [paragraph, setParagraph] = useState('');
+    const mp = false;
 
     function toggleAnimation(element: HTMLElement, animation: string) {
         element.classList.remove(animation);
@@ -68,12 +71,18 @@ export default function Main() {
         <div className='max-w-[75rem] min-w-[50rem] m-16 justify-center content-center'>
             <h1 className='font-bold text-5xl text-[#9FADC6] m-2'>monkeyspeak</h1>
             <div className='w-full flex flex-col justify-center items-center gap-5'>
+                {
+                //  mp? <MultiplayerBars/>: <></>
+                }
                 <div ref={textBox} onAnimationEndCapture={() => { setGameOver(!gameOver); }} className='w-full grid'>
                     {
                         gameOver ?
                             <Results accuracy={30} wpm={30} time={30}/>
                         : 
-                            <TextBox paragraph={paragraph} />
+                            (gameRunning || !mp)?
+                                <TextBox paragraph={paragraph} />
+                            :
+                                <></>
                     }
                 </div>
 
@@ -81,7 +90,11 @@ export default function Main() {
                     {
                         gameRunning ?
                             <h2 className='rounded-[5rem] font-bold text-4xl text-[#9FADC6]'>{timer}</h2>
-                        : <button onClick={() => { gameOver ? toggleAnimation(textBox.current as HTMLDivElement, 'animate-[fade_0.5s]') : startGame(); }} className="w-[16.25rem] h-[6rem] rounded-[1.5rem] font-bold text-3xl text-[#394760] bg-[#9FADC6] p-2 px-5">{gameOver ? 'New Game' : 'Start'}</button>
+                        : 
+                        <div className='flex space-x-24'>
+                            <button onClick={() => { gameOver ? toggleAnimation(textBox.current as HTMLDivElement, 'animate-[fade_0.5s]') : startGame(); }} className="w-[16.25rem] h-[6rem] rounded-[1.5rem] font-bold text-3xl text-[#394760] bg-[#9FADC6] p-2 px-5">{gameOver ? 'New Game' : 'Start'}</button>
+                            <button onClick={() => console.log("Disconnect")} className="w-[16.25rem] h-[6rem] rounded-[1.5rem] font-bold text-3xl text-[#394760] bg-[#9FADC6] p-2 px-5" hidden={!mp} >Disconnect</button>
+                        </div>
                     }
                 </div>
             </div>
