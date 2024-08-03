39284c1fb5e5114e7b7d329801f351d28d2f0a47
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2024-08-03 16:47:41 +1200

    x

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useRef, useState, useEffect } from 'react';
 
@@ -6,77 +6,75 @@ import TextBox from './textbox';
 import Results from './results';
 
 export default function Main() {
-    const textBox = useRef<HTMLDivElement>(null);
-    const [timerInterval, setTimerInterval] = useState<NodeJS.Timeout>();
-    const [gameOver, setGameOver] = useState(false);
-    const [gameRunning, setGameRunning] = useState(false);
-    const [timer, setTimer] = useState(0);
+  const [timerInterval, setTimerInterval] = useState<NodeJS.Timeout>();
+  const [gameOver, setGameOver] = useState(false);
+  const [gameRunning, setGameRunning] = useState(false);
+  const [timer, setTimer] = useState(0);
 
-    function toggleAnimation(element: HTMLElement, animation: string) {
-        element.classList.remove(animation);
-        element.offsetHeight;
-        element.classList.add(animation);
+  function startGame() {
+    setTimer(0);
+
+    setGameRunning(true);
+    setGameOver(false);
+  }
+
+  function endGame() {
+    setGameRunning(false);
+
+    document.removeEventListener('keydown', handleKeyDown);
+  }
+
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.key == 'Escape' && gameRunning) {
+      endGame();
+    }
+  }
+
+  useEffect(() => {
+    if (gameRunning) {
+      document.addEventListener('keydown', handleKeyDown);
+
+      setTimerInterval(
+        setInterval(() => {
+          setTimer((prevTimer) => prevTimer + 1);
+        }, 1000)
+      );
     }
 
-    function startGame() {
-        setTimer(0);
-        
-        setGameRunning(true);
-        setGameOver(false);
-    }
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [gameRunning]);
 
-    function endGame() {
-        setGameRunning(false);
-    }
+  return (
+    <div className="flex flex-col justify-center px-20 grow">
+      <h1 className="font-bold text-5xl text-[#9FADC6] mb-4">monkeyspeak</h1>
 
-    function handleKeyDown(event: KeyboardEvent) {
-        if (event.key == 'Escape' && gameRunning) {
-            toggleAnimation(textBox.current as HTMLDivElement, 'animate-[fade_0.5s]');
+      <div
+        onAnimationEndCapture={() => {
+          setGameOver(!gameOver);
+        }}
+        className="mb-8 grid"
+      >
+        {gameOver ? <Results /> : <TextBox text={'test'} index={0} />}
+      </div>
 
-            endGame();
-
-            document.removeEventListener('keydown', handleKeyDown);
-        }
-    }
-
-    useEffect(() => {
-        if (gameRunning) {
-            document.addEventListener('keydown', handleKeyDown);
-
-            setTimerInterval(
-                setInterval(() => {
-                    setTimer((prevTimer) => prevTimer + 1);
-                }, 1000)
-            );
-        }
-
-        return () => {
-            clearInterval(timerInterval);
-        };
-    }, [gameRunning]);
-
-    return (
-        <div className='w-3/4 min-w-max m-16'>
-            <h1 className='font-bold text-5xl text-[#9FADC6] m-2'>monkeyspeak</h1>
-            
-            <div className='w-full flex flex-col justify-center items-center gap-5'>
-                <div ref={textBox} onAnimationEndCapture={() => { setGameOver(!gameOver); }} className='w-full min-h-[25vh] grid'>
-                    {
-                        gameOver ?
-                            <Results />
-                        : 
-                            <TextBox />
-                    }
-                </div>
-
-                <div className='w-full flex justify-center items-center'>
-                    {
-                        gameRunning ?
-                            <h2 className='rounded-[5rem] font-bold text-4xl text-[#9FADC6]'>{timer}</h2>
-                        : <button onClick={() => { gameOver ? toggleAnimation(textBox.current as HTMLDivElement, 'animate-[fade_0.5s]') : startGame(); }} className='rounded-[3rem] font-bold text-2xl text-[#394760] bg-[#9FADC6] p-2 px-5'>{gameOver ? 'New Game' : 'Start'}</button>
-                    }
-                </div>
-            </div>
-        </div>
-    );
+      <div className="flex justify-center items-center">
+        {gameRunning ? (
+          <h2 className="rounded-[5rem] font-bold text-4xl text-[#9FADC6]">
+            {timer}
+          </h2>
+        ) : (
+          <button
+            onClick={() => {
+              gameOver ? setGameOver(false) : startGame();
+            }}
+            className="rounded-[5rem] font-bold text-2xl text-[#394760] bg-[#9FADC6] p-2 px-5"
+          >
+            {gameOver ? 'Restart' : 'Start'}
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
--- app/components/textbox.tsx
+++ app/components/textbox.tsx
@@ -2,18 +2,16 @@
 
 import { useState } from 'react';
 
-export default function TextBox() {
-  const [text, setText] = useState(
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  );
+interface TextBoxProps {
+  text: string;
+  index: number;
+}
 
+export default function TextBox({ text, index }: TextBoxProps) {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <textarea
-        className="overflow-y-hidden resize-none w-full grow bg-transparent text-justify text-2xl text-[#394760] p-2"
-        readOnly={true}
-        value={text}
-      />
+    <div className="overflow-y-hidden resize-none max-h-[130px] grow bg-transparent text-justify text-2xl text-[#394760] select-none">
+      <span>{text}</span>
+      {text}
     </div>
   );
 }
