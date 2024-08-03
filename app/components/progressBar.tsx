8ce0be0efeec08ff5b9d2e66789484e38933fc05
Yoyo <yoyoc.pata@gmail.com>
2024-08-03 21:48:19 +1200

    implemented progress bars
    brogress par

---
+++ app/components/progressBar.tsx
@@ -0,0 +1,18 @@
+import { Progress } from "@/components/ui/progress"
+
+interface User {
+    playerNumber: number;
+    progress: number;
+}
+
+
+export default function progressBar({ user }: { user: User}) {
+    return (
+        <div className='flex-col p-2'>
+            <h1 className='text-2xl text-[#394760]'>Monkey {user.playerNumber}</h1>
+            <div className='w-full'>
+                <Progress className='rounded-[1.5rem] bg-[#394760]' value={user.progress} />
+            </div>
+        </div>
+    )
+}
