0a910b0fef00932b4925ec50497ed1f6dd9b4ce6
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2024-08-04 15:21:17 +1200

    submodule

--- server/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-
-import * as schema from './schema';
-
-export const db = drizzle(postgres(process.env.POSTGRES_URL!), {
-  schema
-});
