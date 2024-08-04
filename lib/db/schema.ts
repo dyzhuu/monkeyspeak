af235ee53afa4832e86ce611717eb426c1ae3e5c
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2024-08-04 16:45:34 +1200

    pre-render

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,14 +1,27 @@
+import { relations } from 'drizzle-orm';
 import { boolean } from 'drizzle-orm/pg-core';
-import { integer, pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text } from 'drizzle-orm/pg-core';
 
 export const rooms = pgTable('rooms', {
-  id: text('id').primaryKey(),
-  inProgress: boolean('in_progress').notNull().default(false),
+  id: serial('id').primaryKey(),
+  roomId: text('room_id').notNull().unique(),
+  started: boolean('in_progress').notNull().default(false),
   private: boolean('private').notNull().default(false)
 });
 
-export const users = pgTable('users', {
+export const roomsRelations = relations(rooms, ({ many }) => ({
+  sessions: many(sessions)
+}));
+
+export const sessions = pgTable('sessions', {
   id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  email: text('email').notNull()
+  socketId: text('socket_id').notNull().unique(),
+  roomId: text('room_id')
 });
+
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  author: one(rooms, {
+    fields: [sessions.roomId],
+    references: [rooms.roomId]
+  })
+}));
